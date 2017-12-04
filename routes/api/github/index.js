da7a7e5917d1fe9454fc81132ac2d901ce8902ed
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-12-04 15:48:47 -0800

    Did /github/user route

--- routes/api/github/index.js
+++ routes/api/github/index.js
@@ -1,7 +1,9 @@
 const router           = require("express").Router();
 
 const searchRoutes     = require("./search.js");
+const userRoutes      = require("./user.js");
 
-router.use("/search",     searchRoutes);
+router.use("/search", searchRoutes);
+router.use("/user",  userRoutes);
 
 module.exports = router;
