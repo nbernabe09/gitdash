8576186fdc6dc526ba6506ce0fd762f817795ced
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-12-10 18:14:16 -0800

    Push test

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -5,7 +5,6 @@ import ContainerCard from "../ContainerCard";
 
 const Login = props =>
   <ContainerCard title="login">
-    <p>Does this work</p>
     <a href="/auth/github">
     <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
       type="submit"
