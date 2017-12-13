04bcf1c71d3a7c18fda11fca9c1fe4db0c33b771
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-12-13 01:17:10 -0800

    Revert back server

--- services/passport.js
+++ services/passport.js
@@ -11,6 +11,7 @@ passport.serializeUser(function (user, done) {
 });
 
 passport.deserializeUser(function (id, done) {
+  console.log("^^^%$"+id);
   User.findById(id, function (err, user) {
     done(err, user);
   });
