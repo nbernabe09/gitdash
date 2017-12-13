b4e2ab1addd3b89a22a148a40f57f0091efe7887
ALM <ALM@dhcp-v070-158.mobile.uci.edu>
2017-12-12 18:20:35 -0800

    Removed callback in /auth/github

--- server.js
+++ server.js
@@ -87,11 +87,8 @@ app.use(routes);
 app.use(router);
 
 app.get('/auth/github',
-passport.authenticate('github', { scope: ['user:email'] }),
-function (req, res) {
-  // The request will be redirected to GitHub for authentication, so this
-  // function will not be called.
-});
+  passport.authenticate('github', { scope: ['user:email'] })
+);
 
 app.get('/auth/github/callback',
 passport.authenticate('github', { failureRedirect: '/login' }),
