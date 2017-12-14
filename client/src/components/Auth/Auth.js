a33e5360db0b60f3a6a43e4ccf6b0baee060bcda
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-12-13 23:14:53 -0800

    Rebuild

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -2,21 +2,16 @@ import React, { Component } from "react";
 import "./Auth.css";
 import ContainerCard from "../ContainerCard";
 import axios from 'axios';
-import { Redirect } from 'react-router';
-
-function refreshPage() {
-  window.location.reload();
-}
 
 class Auth extends Component {
-  componentDidMount() {
-    refreshPage();
-    refreshPage();
-    refreshPage();
+  signInUser() {
+    axios.get('/auth/github').then(res => console.log(res.data));
   }
 
   render() {
-    return <div />
+    return (
+      <div>{this.signInUser()}</div>
+    )
   }
 }
 
