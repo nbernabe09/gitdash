f4863e8940afaa57159ca22c99c0043d7dd57658
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-12-13 17:25:37 -0800

    Change server

--- server.js
+++ server.js
@@ -49,8 +49,14 @@ function (req, res) {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
 } else {
   app.use(express.static("client/public"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"))
+  })
 }
 
 app.listen(PORT, function() {
