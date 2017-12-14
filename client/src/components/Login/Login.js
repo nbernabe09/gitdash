0ffc0129946d4f8fe427f98a930db6c7e2e9bdc4
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-12-13 18:38:34 -0800

    Change server

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -2,20 +2,19 @@ import React, { Component } from "react";
 import "./Login.css";
 import ContainerCard from "../ContainerCard";
 import axios from 'axios';
+import { Redirect } from 'react-router';
 
 class Login extends Component {
   render() {
     return (
-      <ContainerCard title="login">
-          <a href="/auth/github">
+      <ContainerCard title="login">>
             <button
               className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-              onClick={() => this.signInUser()}
+              onClick={() => <Redirect to="/auth/github" />}
               data-upgraded=",MaterialButton,MaterialRipple"
             >
             Login
             </button>
-          </a>
       </ContainerCard>
     )
   }
