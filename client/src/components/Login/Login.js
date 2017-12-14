6f6ec5934ce36abd1eaee2a95c40313501a55864
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-12-13 18:51:50 -0800

    Rebuild

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,11 +3,13 @@ import "./Login.css";
 import ContainerCard from "../ContainerCard";
 import axios from 'axios';
 import { Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
   render() {
     return (
       <ContainerCard title="login">
+          <Link to="/auth/github">
             <button
               className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
               onClick={() => <Redirect to="/auth/github" />}
@@ -15,6 +17,7 @@ class Login extends Component {
             >
             Login
             </button>
+          </Link>
       </ContainerCard>
     )
   }
