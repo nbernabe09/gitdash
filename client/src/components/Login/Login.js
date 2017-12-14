4f3e9d62e0a6d7a6c2c8d16e16fbe6c5b686b099
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-12-13 16:52:42 -0800

    Rebuild

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./Login.css";
 import ContainerCard from "../ContainerCard";
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
 
@@ -11,15 +12,14 @@ class Login extends Component {
   render() {
     return (
       <ContainerCard title="login">
+        <Link to="/auth/github">
           <button
             className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-            onClick={() => this.signInUser()}
-            data-upgraded=",MaterialButton,MaterialRipple"
           >
-          Login
+            Login
           </button>
+        </Link>
       </ContainerCard>
-
     )
   }
 }
