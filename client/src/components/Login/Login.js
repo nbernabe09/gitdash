4bd9a3a0dd68ef9f69b15f052afc9b91e9e58038
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-12-13 18:10:21 -0800

    Rebuild

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -4,22 +4,19 @@ import ContainerCard from "../ContainerCard";
 import axios from 'axios';
 
 class Login extends Component {
-
-  signInUser() {
-    axios.get('/auth/github').then(res => console.log(res.data));
-  }
   render() {
     return (
       <ContainerCard title="login">
-          <button
-            className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-            onClick={() => this.signInUser()}
-            data-upgraded=",MaterialButton,MaterialRipple"
-          >
-          Login
-          </button>
+          <a href="/auth/github">
+            <button
+              className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+              onClick={() => this.signInUser()}
+              data-upgraded=",MaterialButton,MaterialRipple"
+            >
+            Login
+            </button>
+          </a>
       </ContainerCard>
-
     )
   }
 }
