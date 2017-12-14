d66d7c38749506781f71fa83601ced310e999db4
almaclaine@gmail.com <B*4vkmBg[yGaiV,q6(7je[GK6^nhfAqwL8#)h8n7u8,gE93,xw>
2017-12-13 17:48:46 -0800

    Change server

--- server.js
+++ server.js
@@ -45,11 +45,8 @@ app.get('/auth/github', (req, res, next) => {
 }));
 
 app.get('/auth/github/callback', (req, res, next) => {
-  console.log("CALLBACK");
-  console.log(req.query);
-  console.log(req.user);
-  if(req.user) res.redirect('/')
-  else next();
+  if(!req.user) next();
+  else res.redirect("/");
 },
 passport.authenticate('github', { failureRedirect: '/login' }));
 
